938c7f84448c2a2f6905125f7e76986266903d5f
smittycent <smittycent@322566ba-e0e5-11dd-a738-5fe2e3cfbcc5>
2009-03-10 06:18:23 +0000

    ShiftLeft

---
+++ trunk/src/llvm/ShiftLeftInstruction.java
@@ -0,0 +1,43 @@
+package llvm;
+
+/**
+ * llvm: ShiftLeftInstruction
+ * <p/>
+ * Description:
+ *
+ * @author Patrick J. Smith
+ * @date Mar 9, 2009
+ */
+public class ShiftLeftInstruction extends LLVMInstruction {
+    private int value;
+    private int numShifts;
+
+    public ShiftLeftInstruction(int target, int value, int numShifts)
+    {
+        super(target, "i32");
+        this.value = value;
+        this.numShifts = numShifts;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getNumShifts() {
+        return numShifts;
+    }
+
+    public void setNumShifts(int numShifts) {
+        this.numShifts = numShifts;
+    }
+
+    public String toString()
+    {
+        String s = "%r" + super.getTargetRegister() + " = shl " + super.getType() + "%r" + value + ", " + numShifts;
+        return s;
+    }
+}
